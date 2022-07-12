0708b806127dc399b6bf5e4f65d5216fe8cc7607
zm102905 <zm102905@gmail.com>
2022-07-12 14:45:48 -0400

    populator. needs work

--- application/src/main/java/org/wecancodeit/serverside/Populator.java
+++ application/src/main/java/org/wecancodeit/serverside/Populator.java
@@ -1,6 +1,30 @@
 package org.wecancodeit.serverside;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+import javax.annotation.Resource;
+
+@Component
+public class Populator implements CommandLineRunner{
+
+    @Resource
+    private FishRepository fishRepo;
+
+    @Resource
+    git c
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        Fish atlanticSalmon = new Fish("Atlantic Salmon", "");
+        fishRepo.save(atlanticSalmon);
+        Fish bluefish = new Fish("Bluefish", "");
+        fishRepo.save(bluefish);
+        Fish cobia = new Fish("Cobia", "");
+        fishRepo.save(cobia);
+        Fish haddock = new Fish("haddock", "");
+        fishRepo.save(haddock);
+        fish opah = new Fish("Opah", "");
 
 
-
-public class Populator {
+    }
 }
