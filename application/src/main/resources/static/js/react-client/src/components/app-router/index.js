19e6502a5f3c41b683b04c818644175db3869c6d
faridane <faridandiaye60@gmail.com>
2022-07-14 14:21:57 -0400

    update

--- application/src/main/resources/static/js/react-client/src/components/app-router/index.js
+++ application/src/main/resources/static/js/react-client/src/components/app-router/index.js
@@ -3,6 +3,7 @@ import { Route, Switch } from 'react-router-dom';
 import ContactScreen from '../../pages/contact';
 import HomeScreen from '../../pages/home-screen';
 import Mystery from '../../pages/mystery-educator';
+// import Comment from '../../pages/comment';
 import React from 'react';
 
 const AppRouter = () => {
@@ -11,7 +12,7 @@ const AppRouter = () => {
             <Route exact path={'/'} component={HomeScreen} />
             <Route exact path={'/contact'} component={ContactScreen} />
             <Route exact path={'/mystery'} component={Mystery} />
-            {/* <Route exact path={'/about'} component={About} /> */}
+            {/* <Route exact path={'/comment'} component={Comment} /> */}
         </Switch>
     );
 }
--- application/src/main/resources/static/js/react-client/src/pages/fish/index.js
+++ application/src/main/resources/static/js/react-client/src/pages/fish/index.js
@@ -1,12 +1,84 @@
-import React from 'react';
-import style from './style.module.scss';
+// import React, { useState } from 'react';
 
-const Fish = () =>{
+// import axios from 'axios';
+// import style from './style.module.scss';
 
-    return (
-        <div className={style.fish}>
-            <a href='https://www.fishwatch.gov/api/species'></a>
-        </div>
-    );
-}
-export default Fish;
+// const Fishs = ({ fishs }) => {
+
+//     const [fishsState, setFishsState] = useState(fishs);
+
+//     const [fishState, setFishState] = useState({
+//         name: "",
+//         isSelected: false
+//     });
+
+//     const handleChange = (e) => {
+//         const value = e.target.value;
+//         setFishState({
+//             ...fishState,
+//             [e.target.name]: value
+//         });
+//     };
+
+//     const handleSubmit = (e) => {
+//         e.preventDefault();
+
+//         const userData = {
+//             name: fishState.name,
+//             isSelected: false
+//         };
+
+//         axios.post('http://localhost:8080/api/fishs/add-fish', userData).then((response) => {
+//             console.log(response.status);
+//             console.log('DATA', response.data);
+//             setFishsState(response.data);
+//         });
+//     };
+
+//     const handleDelete = (fishId) => {
+//         axios.delete(`http://localhost:8080/api/fishs/${fishId}/delete-fish`).then((response) => {
+//             console.log('Delete successful');
+//             console.log('DATA', response.data);
+//             setFishsState(response.data);
+//         });
+//     }
+
+//     const handleSelectUpdate = (fishId, fishName) => {
+//         const userData = {
+//             name: fishName,
+//             isSelected: true
+//         }
+//         axios.put(`http://localhost:8080/api/fishs/${fishId}/select-fish`, userData).then((response) => {
+//             console.log('Update successful');
+//             console.log('DATA', response.data);
+//             setFishsState(response.data);
+//         });
+//     }
+
+//     return (
+//         <div className={style.fishForm}>
+//             <form onSubmit={handleSubmit}>
+//                 <input
+//                     type="text"
+//                     name="name"
+//                     value={fishState.name}
+//                     onChange={handleChange}
+//                     placeholder='Add a fish here'
+//                 />
+//                 <button type="submit">Add fish type</button>
+//             </form>
+//             <h3>Your fish</h3>
+//             <div className={style.fishsContainer}>
+//                 {fishsState.map(fish => (
+//                     <div className={style.fishContainer} key={fish.id}>
+//                         <p className={fish.selected ? style.selected : null} onClick={() => handleSelectUpdate(fish.id, fish.name)}>{fish.name}<button onClick={() => handleDelete(fish.id)}>x</button></p>
+//                     </div>
+//                 ))}
+//             </div>
+//         </div>
+
+//     );
+// }
+
+// export default Fishs;
+
