082788d4caab18cc6404d68aa179db1a506a8ba1
JNegron300 <metal_x40@live.com>
2022-07-14 18:51:37 -0400

    aboutpage

--- application/src/main/resources/static/js/react-client/src/components/app-router/index.js
+++ application/src/main/resources/static/js/react-client/src/components/app-router/index.js
@@ -1,12 +1,18 @@
 import { Route, Switch } from 'react-router-dom';
-import Jac from '../../pages/jac';
+
+import AboutPage from '../../pages/about-us';
 import CommentScreen from '../../pages/comment';
-import HomeScreen from '../../pages/home-screen';
-// import About from '../../pages/about-us';
-import Mystery from '../../pages/mystery-educator';
 import Fish from '../../pages/fish';
-// import SolarSystem from '../../pages/solar-system';
+import HomeScreen from '../../pages/home-screen';
+import Jac from '../../pages/jac';
+import Mystery from '../../pages/mystery-educator';
 import React from 'react';
+
+// import About from '../../pages/about-us';
+
+
+// import SolarSystem from '../../pages/solar-system';
+
 
 const AppRouter = () => {
     return (
@@ -17,7 +23,7 @@ const AppRouter = () => {
             {/* <Route exact path={'/solar-system'} component={SolarSystem} /> */}
             <Route exact path={'/mystery'} component={Mystery} />
             <Route exact path={'/jac'} component={Jac} />
-            {/* <Route exact path={'/about-us'} component={About} /> */}
+            <Route exact path={'/about'} component={AboutPage} />
         </Switch>
     );
 }
