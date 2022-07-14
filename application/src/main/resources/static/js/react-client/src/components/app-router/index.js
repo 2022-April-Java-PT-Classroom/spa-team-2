f87e5b845fcb071c98c77845668ce1ed4e769c2b
mark-sypniewski <msyp721@gmail.com>
2022-07-14 14:18:10 -0400

    comment and Jac pages setup

--- application/src/main/resources/static/js/react-client/src/components/app-router/index.js
+++ application/src/main/resources/static/js/react-client/src/components/app-router/index.js
@@ -1,6 +1,7 @@
 import { Route, Switch } from 'react-router-dom';
 
-import ContactScreen from '../../pages/contact';
+import Jac from '../../pages/jac';
+import CommentScreen from '../../pages/comment';
 import HomeScreen from '../../pages/home-screen';
 import Mystery from '../../pages/mystery-educator';
 import React from 'react';
@@ -9,7 +10,8 @@ const AppRouter = () => {
     return (
         <Switch>
             <Route exact path={'/'} component={HomeScreen} />
-            <Route exact path={'/contact'} component={ContactScreen} />
+            <Route exact path={'/comment'} component={CommentScreen} />
+            <Route exact path={'/jac'} component={Jac} />
             <Route exact path={'/mystery'} component={Mystery} />
             {/* <Route exact path={'/about'} component={About} /> */}
         </Switch>
--- application/src/main/resources/static/js/react-client/src/pages/comment/index.js
+++ application/src/main/resources/static/js/react-client/src/pages/comment/index.js
@@ -1,4 +1,17 @@
-import React, { useState } from 'react';
-
-import axios from 'axios';
+import React from 'react';
+import phoneIcon from '../../assets/contact.png';
 import style from './style.module.scss';
+
+const CommentScreen = () => {
+    return (
+        <div className={style.comment}>
+            <h2><img src={phoneIcon} alt='phone icon' />Contact Us Today!</h2>
+            <h4>(555) 555-5555</h4>
+            <br></br>
+            <h3>Comment<textarea rows={10} cols={30} placeholder='Leave a comment here with your Name and contact please!'></textarea></h3>
+           
+        </div>
+    );
+}
+
+export default CommentScreen;
