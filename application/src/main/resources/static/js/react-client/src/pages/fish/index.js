063802dc762fa4f43df5a4991a6ebc7f5740a273
zm102905 <zm102905@gmail.com>
2022-07-15 15:14:28 -0400

    changes some of the styling on the fish page

--- application/src/main/resources/static/js/react-client/src/pages/fish/index.js
+++ application/src/main/resources/static/js/react-client/src/pages/fish/index.js
@@ -18,27 +18,27 @@ const Fish = () => {
                 <h2>This site offers lots of cool facts about the many fish that inhabit the waters of our world. Read below to learn more!</h2>
             </div>
             <div className={style.fishWrapper}>
-                <div className={style.section2}>
+                <div className={style.section}>
                     <h2>Atlantic Salmon</h2>
                     <img className={style.fishImage} src={salmonImage} alt='salmon image' />
                     <p>Information about atlantic salmon from FishWatch</p> 
                 </div>
-                <div className={style.section3}>  
+                <div className={style.section}>  
                     <h2>Bluefish</h2>
                     <img className={style.fishImage} src={bluefishImage} alt='bluefish image' /> 
                     <p>Information about bluefish from FishWatch</p>
                 </div>
-                <div className={style.section4}>
+                <div className={style.section}>
                     <h2>Cobia</h2>
                     <img className={style.fishImage} src={cobiaImage} alt='cobia image' /> 
                     <p>Information about cobia from Fishwatch</p>
                 </div>
-                <div className={style.section5}>
+                <div className={style.section}>
                     <h2>Haddock</h2>
                     <img className={style.fishImage} src={haddockImage} alt='haddock image' /> 
                     <p>Information about haddock from FishWatch</p>
                 </div>
-                <div className={style.section6}>
+                <div className={style.section}>
                     <h2>Opah</h2>
                     <img className={style.fishImage} src={opahImage} alt='opah image' /> 
                     <p>Information about opah from FishWatch</p>
