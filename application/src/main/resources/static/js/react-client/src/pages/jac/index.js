e997383696c2664cb5f001287b7e1f7918bac95e
mark-sypniewski <msyp721@gmail.com>
2022-07-15 13:35:40 -0400

    JAC linked, needs styling

--- application/src/main/resources/static/js/react-client/src/pages/jac/index.js
+++ application/src/main/resources/static/js/react-client/src/pages/jac/index.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import style from './style.module.scss';
+import jacImage from '../../assets/jac.jpeg';
 
+               
 const Jac = () => {
-    return (
-        <div className={style.contactus}>
-            <h2>Route To Jac</h2>
-            <br></br>
-        </div>
-    );
+return (
+       
+   <div className={style.mystere}>
+        <div className={style.container}>
+            
+            <div className={style.section1}>
+                <h2>Jac Enterprises</h2>
+                <a href='http://127.0.0.1:5500/index.html' target="_blank"><img src={jacImage} alt='space image' /></a> 
+            </div>
+      </div>
+    </div>  
+   )
 }
 
 export default Jac;
