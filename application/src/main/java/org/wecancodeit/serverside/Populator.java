4f90d1f4572183d8ef80936a5dcf5088f7326a1a
zm102905 <zm102905@gmail.com>
2022-07-18 21:07:05 -0400

    work in progress

--- application/src/main/java/org/wecancodeit/serverside/Populator.java
+++ application/src/main/java/org/wecancodeit/serverside/Populator.java
@@ -27,6 +27,7 @@ public class Populator implements CommandLineRunner{
         Fish haddock = new Fish("Haddock", "Haddock are found on both sides of the North Atlantic. In the western North Atlantic, they’re found from Newfoundland to Cape May, New Jersey, and are most abundant on Georges Bank and in the Gulf of Maine.", "Haddock are a fast-growing species that typically range between 1 and 3 feet long at maturity. They generally weigh between 2 and 7 pounds. Haddock begin to reproduce between the ages of 1 and 4 years old and at 10.5 to 11.7 inches long.", "Haddock are a member of the cod family, but they are smaller than Atlantic cod. They can be distinguished by a black “thumbprint” found on each side of their body.");
         fishRepo.save(haddock);
         Fish opah = new Fish("Opah", "Opah are found in tropical and temperate waters all around the world", "Because opah are not a major commercial seafood species and they live in the deep ocean, scientists know very little about their biology and ecology.", "Opah are an unusual looking fish—they have a round, flat body that’s silvery gray in color. Toward the belly, the silver shades to a rose red, dotted with white spots.");
+        fishRepo.save(opah);
 
         Planet mars = new Planet("Mars", "Red planet", "oblate spheroid");
         planetRepo.save(mars);
--- application/src/main/resources/static/js/react-client/src/pages/fish/index.js
+++ application/src/main/resources/static/js/react-client/src/pages/fish/index.js
@@ -1,70 +1,82 @@
-
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import style from './fish.module.scss';
-import opahImage from '../../assets/opah.png';
-import salmonImage from '../../assets/salmon.png';
 import bluefishImage from '../../assets/bluefish.png';
 import haddockImage from '../../assets/haddock.png';
 import cobiaImage from '../../assets/cobia.png';
-import spaceImage1 from '../../assets/fish.jpg';
 import fishbackground from '../../assets/fishbackground.jpg';
+import haddockImage from '../../assets/haddock.png';
+import opahImage from '../../assets/opah.png';
+import salmonImage from '../../assets/salmon.png';
+import style from './fish.module.scss';
+
 const Fish = () => {
+
+    const [loading, setLoading] = useState(true),
+        [fishData, setFishData] = useState(null);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const result = await axios('/api/fish');
+            setFishData(result.data);
+        }
+        fetchData()
+    }, []);
+
     return (
-        <div  className={style.fish}>
-        
+        <div className={style.fish}>
+
             <div className={style.title}>
                 <div className={style.head}>
-                <h1>Come explore the oceans!</h1>
+                    <h1>Come explore the oceans!</h1>
                 </div>
                 <div className={style.info}>
-                <h3>This site offers lots of cool facts about the many fish that inhabit the waters of our world. Read below to learn more!</h3>
+                    <h3>This site offers lots of cool facts about the many fish that inhabit the waters of our world. Read below to learn more!</h3>
                 </div>
             </div>
             <div className={style.fishWrapper}>
-                <div className={style.section}>
-                <div className={style.head}>
-                    <h2>Atlantic Salmon</h2>
+                <div className={style.sections}>
+                    <div className={style.head}>
+                        <h2>Atlantic Salmon</h2>
                     </div>
                     <img className={style.fishImage} src={salmonImage} alt='salmon image' />
                     <div className={style.info}>
-                    <p>Information about atlantic salmon from FishWatch</p> 
+                        <p>Information about atlantic salmon from FishWatch</p>
                     </div>
                 </div>
-                <div className={style.section}> 
-                <div className={style.head}>
-                    <h2>Bluefish</h2>
-                </div>
-                    <img className={style.fishImage} src={bluefishImage} alt='bluefish image' /> 
+                <div className={style.sections}>
+                    <div className={style.head}>
+                        <h2>Bluefish</h2>
+                    </div>
+                    <img className={style.fishImage} src={bluefishImage} alt='bluefish image' />
                     <div className={style.info}>
-                    <p>Information about bluefish from FishWatch</p>
+                        <p>Information about bluefish from FishWatch</p>
                     </div>
                 </div>
-                <div className={style.section}>
-                <div className={style.head}>
-                    <h2>Cobia</h2>
+                <div className={style.sections}>
+                    <div className={style.head}>
+                        <h2>Cobia</h2>
                     </div>
-                    <img className={style.fishImage} src={cobiaImage} alt='cobia image' /> 
+                    <img className={style.fishImage} src={cobiaImage} alt='cobia image' />
                     <div className={style.info}>
-                    <p>Information about cobia from Fishwatch</p>
+                        <p>Information about cobia from Fishwatch</p>
                     </div>
                 </div>
-                <div className={style.section}>
-                <div className={style.head}>
-                    <h2>Haddock</h2>
+                <div className={style.sections}>
+                    <div className={style.head}>
+                        <h2>Haddock</h2>
                     </div>
-                    <img className={style.fishImage} src={haddockImage} alt='haddock image' /> 
+                    <img className={style.fishImage} src={haddockImage} alt='haddock image' />
                     <div className={style.info}>
-                    <p>Information about haddock from FishWatch</p>
+                        <p>Information about haddock from FishWatch</p>
                     </div>
                 </div>
-                <div className={style.section}>
-                <div className={style.head}>
-                    <h2>Opah</h2>
-                </div>
-                    <img className={style.fishImage} src={opahImage} alt='opah image' /> 
+                <div className={style.sections}>
+                    <div className={style.head}>
+                        <h2>Opah</h2>
+                    </div>
+                    <img className={style.fishImage} src={opahImage} alt='opah image' />
                     <div className={style.info}>
-                    <p>Information about opah from FishWatch</p>
+                        <p>Information about opah from FishWatch</p>
                     </div>
                 </div>
             </div>
